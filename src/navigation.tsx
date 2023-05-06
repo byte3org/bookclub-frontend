d37cf78625c295ba89ae8d7c16fac7d42ee45839
rxOred <rxored@gmail.com>
2023-05-06 18:51:10 +0530

    feat: added auth navigation

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import SignIn from "./screens/signIn";
+import SignUp from "./screens/signUp";
+
 const Stack = createNativeStackNavigator();
-
-
 
 export function AuthNavigation() {
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }} >
-
+            <Stack.Screen name={"signin"} component={SignIn}></Stack.Screen>
+            <Stack.Screen name={"signup"} component={SignUp}></Stack.Screen>
         </Stack.Navigator>
     )
 }
