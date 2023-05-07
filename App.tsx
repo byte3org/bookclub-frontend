cb68c07b7a2bbb003660bb3c5816170e9da9ec24
rxOred <rxored@gmail.com>
2023-05-07 13:25:38 +0530

    changes to be commited

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import AppContainer from './src/appContainer';
 import { AuthProvider } from "./src/authContext";
 
 export default function App() {
   return (
-    <></>
+    <AuthProvider>
+        <AppContainer/>
+    </AuthProvider>
   );
 }
 
